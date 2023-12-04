3365ed486f17b18ca38cc089cc3e580112b843f6
Rachit Garg <rachitgarg900@gmail.com>
2023-12-04 07:20:05 +0100

    Added Cli DS

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,8 +1,18 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf
+}
+
 fn main() {
-    
+
     // Expect: grrs <pattern> <path>
     let pattern = std::env::args().nth(1).expect("No pattern given");
     let path = std::env::args().nth(2).expect("No path given");
 
-    println!("Pattern: {:?}\nPath: {:?}", pattern, path);
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path)
+    };
+
+    println!("Pattern: {:?}\nPath: {:?}", args.pattern, args.path);
 }
