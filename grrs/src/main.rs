6c5bbeb9016c347ae8702ef49333d27e84e182c0
Rachit Garg <rachitgarg900@gmail.com>
2023-12-04 07:01:22 +0100

    Added command line parsing using std::env::args()

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    
+    // Expect: grrs <pattern> <path>
+    let pattern = std::env::args().nth(1).expect("No pattern given");
+    let path = std::env::args().nth(2).expect("No path given");
+
+    println!("Pattern: {:?}\nPath: {:?}", pattern, path);
 }
