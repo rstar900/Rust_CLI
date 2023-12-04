b74b8fd3859e8c5fa22d25a909f17d24fd42688f
Rachit Garg <rachitgarg900@gmail.com>
2023-12-04 07:40:25 +0100

    replaced manual argument parsing with clap

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,18 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
+    // Pattern string to look for
     pattern: String,
+    // Path of the file, in which the pattern is to be searched
     path: std::path::PathBuf
 }
 
 fn main() {
 
     // Expect: grrs <pattern> <path>
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("No path given");
-
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path)
-    };
+    // Instead of using std::env::args().nth(n).expect("message") and parsing manually,
+    // we use clap's parsing functionality
+    let args = Cli::parse();
 
     println!("Pattern: {:?}\nPath: {:?}", args.pattern, args.path);
 }
